c341f3e1a5ad655cfae7ab31ad52b42fe0f03f1c
aepii <simentalma@yahoo.com>
2025-01-23 14:36:15 -0600

    Structured API endpoint for Products.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,30 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js"
 
+dotenv.config();
 const app = express();
 
-app.get("/products", (req, res) => {
-  res.send("Server is ready");
+app.post("/products", async (req, res) => {
+  const product = req.body;
+
+  if(!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product)
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    return res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 })
 
 app.listen(5000, () => {
+  connectDB();
   console.log("Server started at http://localhost:5000");
 })
