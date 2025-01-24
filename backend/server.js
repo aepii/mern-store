d34889778a6b323a0ccd1b4dab23b89b4ab10eee
aepii <simentalma@yahoo.com>
2025-01-23 19:36:41 -0600

    Modularized API structure and added CRUD operations.

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js"
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 const app = express();
 
-app.post("/products", async (req, res) => {
-  const product = req.body;
+app.use(express.json());
 
-  if(!product.name || !product.price || !product.image) {
-    return res.status(400).json({ success: false, message: "Please provide all fields" });
-  }
-
-  const newProduct = new Product(product)
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    return res.status(500).json({ success: false, message: "Internal Server Error" });
-  }
-})
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
   console.log("Server started at http://localhost:5000");
-})
+});
